6f37867fb4ae7dcb64300bbd12583d2783623b57
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2023-04-13 18:20:27 +0530

    sudoku-solver

--- sudoku.js
+++ sudoku.js
@@ -1,72 +1,180 @@
-// only numbers between 1 and 9 can be entered into td
-document.getElementById("board").addEventListener("keyup", function (e) {
-  if (e.target && e.target.nodeName == "TD") {
-    var valid_number = /[1-9]/;
-    var tdElement = e.target;
-    if (
-      tdElement.innerText.length > 0 &&
-      valid_number.test(tdElement.innerText[0])
-    ) {
-      tdElement.innerText = tdElement.innerText[0];
-    } else {
-      tdElement.innerText = "";
+var TESTABLE = true;
+
+var SudokuSolver = (function (testable) {
+  var solver;
+
+  // PUBLIC FUNCTIONS
+  function solve(boardString) {
+    var boardArray = boardString.split("");
+    if (boardIsInvalid(boardArray)) {
+      return false;
+    }
+    return recursiveSolve(boardString);
+  }
+
+  function solveAndPrint(boardString) {
+    var solvedBoard = solve(boardString);
+    console.log(toString(solvedBoard.split("")));
+    return solvedBoard;
+  }
+
+  // PRIVATE FUNCTIONS
+  function recursiveSolve(boardString) {
+    var boardArray = boardString.split("");
+    if (boardIsSolved(boardArray)) {
+      return boardArray.join("");
+    }
+    var cellPossibilities = getNextCellAndPossibilities(boardArray);
+    var nextUnsolvedCellIndex = cellPossibilities.index;
+    var possibilities = cellPossibilities.choices;
+    for (var i = 0; i < possibilities.length; i++) {
+      boardArray[nextUnsolvedCellIndex] = possibilities[i];
+      var solvedBoard = recursiveSolve(boardArray.join(""));
+      if (solvedBoard) {
+        return solvedBoard;
+      }
+    }
+    return false;
+  }
+
+  function boardIsInvalid(boardArray) {
+    return !boardIsValid(boardArray);
+  }
+
+  function boardIsValid(boardArray) {
+    return (
+      allRowsValid(boardArray) &&
+      allColumnsValid(boardArray) &&
+      allBoxesValid(boardArray)
+    );
+  }
+
+  function boardIsSolved(boardArray) {
+    for (var i = 0; i < boardArray.length; i++) {
+      if (boardArray[i] === "-") {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function getNextCellAndPossibilities(boardArray) {
+    for (var i = 0; i < boardArray.length; i++) {
+      if (boardArray[i] === "-") {
+        var existingValues = getAllIntersections(boardArray, i);
+        var choices = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].filter(
+          function (num) {
+            return existingValues.indexOf(num) < 0;
+          }
+        );
+        return { index: i, choices: choices };
+      }
     }
   }
-});
 
-// document.getElementById("clear").addEventListener("click", function (e) {
-//   // var boardString = boardToString();
-//   var solution = sudoku.solve(boardString);
-//   if (solution) {
-//     stringToBoard(solution);
-//   } else {
-//     alert("invalid sudoku");
-//   }
-// });
-
-//convert all the data in the cells to a string
-function boardToString() {
-  var string = "";
-  var valid_number = /[1-9]/;
-  var datacells = document.getElementsByTagName("td");
-  for (var i = 0; i < datacells.length(); i++) {
-    if (valid_number.test(datacells[i].innerText[0])) {
-      string += datacells[i].innerText;
-    } else {
-      string += "-";
-    }
+  function getAllIntersections(boardArray, i) {
+    return getRow(boardArray, i)
+      .concat(getColumn(boardArray, i))
+      .concat(getBox(boardArray, i));
   }
-}
-function stringToBoard(string) {
-  var currentCell;
-  var valid_number = /[1-9]/;
-  var cells = string.split("");
-  var datacells = document.getElementsByTagName("td");
-  for (var i = 0; i < datacells.length(); i++) {
-    currentCell = cells.shift();
-    if (valid_number.test(currentCell)) {
-      datacells.innerText = currentCell;
-    }
+
+  function allRowsValid(boardArray) {
+    return [0, 9, 18, 27, 36, 45, 54, 63, 72]
+      .map(function (i) {
+        return getRow(boardArray, i);
+      })
+      .reduce(function (validity, row) {
+        return collectionIsValid(row) && validity;
+      }, true);
   }
-}
 
-function boardIsInvalid(boardArray) {
-  return boardIsValid(boardArray);
-}
-function boardIsValid(boardArray) {
-  return (
-    allRowsValid(boardArray) &&
-    allColumnsValid(boardArray) &&
-    allBoxesValid(boardArray)
-  );
-}
+  function getRow(boardArray, i) {
+    var startingEl = Math.floor(i / 9) * 9;
+    return boardArray.slice(startingEl, startingEl + 9);
+  }
 
-function allRowsValid(boardArray) {
-  return [0, 9, 18, 27, 36, 45, 63, 72]
-    .map(function (i) {
-      return getRow(boardArray, i);
-    })
-    .reduce(function (validity, row) {
-      return collectionValid(row) && validity;
-    }, true);
-}
+  function allColumnsValid(boardArray) {
+    return [0, 1, 2, 3, 4, 5, 6, 7, 8]
+      .map(function (i) {
+        return getColumn(boardArray, i);
+      })
+      .reduce(function (validity, row) {
+        return collectionIsValid(row) && validity;
+      }, true);
+  }
+
+  function getColumn(boardArray, i) {
+    var startingEl = Math.floor(i % 9);
+    return [0, 1, 2, 3, 4, 5, 6, 7, 8].map(function (num) {
+      return boardArray[startingEl + num * 9];
+    });
+  }
+
+  function allBoxesValid(boardArray) {
+    return [0, 3, 6, 27, 30, 33, 54, 57, 60]
+      .map(function (i) {
+        return getBox(boardArray, i);
+      })
+      .reduce(function (validity, row) {
+        return collectionIsValid(row) && validity;
+      }, true);
+  }
+
+  function getBox(boardArray, i) {
+    var boxCol = Math.floor(i / 3) % 3;
+    var boxRow = Math.floor(i / 27);
+    var startingIndex = boxCol * 3 + boxRow * 27;
+    return [0, 1, 2, 9, 10, 11, 18, 19, 20].map(function (num) {
+      return boardArray[startingIndex + num];
+    });
+  }
+
+  function collectionIsValid(collection) {
+    var numCounts = {};
+    for (var i = 0; i < collection.length; i++) {
+      if (collection[i] != "-") {
+        if (numCounts[collection[i]] === undefined) {
+          numCounts[collection[i]] = 1;
+        } else {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  function toString(boardArray) {
+    return [0, 9, 18, 27, 36, 45, 54, 63, 72]
+      .map(function (i) {
+        return getRow(boardArray, i).join(" ");
+      })
+      .join("\n");
+  }
+
+  if (testable) {
+    // These methods will be exposed publicly when testing is on.
+    solver = {
+      solve: solve,
+      solveAndPrint: solveAndPrint,
+      recursiveSolve: recursiveSolve,
+      boardIsInvalid: boardIsInvalid,
+      boardIsValid: boardIsValid,
+      boardIsSolved: boardIsSolved,
+      getNextCellAndPossibilities: getNextCellAndPossibilities,
+      getAllIntersections: getAllIntersections,
+      allRowsValid: allRowsValid,
+      getRow: getRow,
+      allColumnsValid: allColumnsValid,
+      getColumn: getColumn,
+      allBoxesValid: allBoxesValid,
+      getBox: getBox,
+      collectionIsValid: collectionIsValid,
+      toString: toString,
+    };
+  } else {
+    // These will be the only public methods when testing is off.
+    solver = { solve: solve, solveAndPrint: solveAndPrint };
+  }
+
+  return solver;
+})(TESTABLE);
